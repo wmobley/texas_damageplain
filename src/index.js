b7b7a916fa282ed69441115c5b4ca25b94d04e46
Will Mobley <feulmob+github@gmail.com>
2023-09-12 07:53:45 -0500

    Updated Map Prod

--- src/index.js
+++ src/index.js
@@ -1,31 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
+import ReactDOM from 'react-dom/client';
 import './index.css';
-import {BrowserRouter, Redirect, Route, Switch,} from "react-router-dom";
+import {BrowserRouter, Redirect, Route, Router, Switch,} from "react-router-dom";
 import Hazard from "./views/Hazard";
+
 // import "@fortawesome/fontawesome-free/css/all.min.css";
 import reportWebVitals from './reportWebVitals';
+const rootElement = document.getElementById("root")
 
-ReactDOM.render(
-    <BrowserRouter>
-        <Switch>
-            {/* add routes with layouts */}
-            {/*<Route path="/admin" component={Admin} />*/}
-            {/*<Route path="/auth" component={Auth} />*/}
-            {/*/!* add routes without layouts *!/*/}
-            {/*<Route path="/landing" exact component={Landing} />*/}
-            {/*<Route path="/profile" exact component={Profile} />*/}
-            <Route path="/" exact component={Hazard} />
-            <Route path="/Tools"  component={Hazard} />
-
-            {/*<Route path="/Data" exact component={Data} />*/}
-            {/*<Route path="/Current" exact component={Current} />*/}
-            {/* add redirect for first page */}
-            <Redirect from="*" to="/" />
-        </Switch>
-    </BrowserRouter>,
-    document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <Hazard/>
+    </React.StrictMode>
 );
+    {/*    <Switch>*/}
+    {/*//         /!* add routes with layouts *!/*/}
+    {/*//         /!*<Route path="/admin" component={Admin} />*!/*/}
+    {/*//         /!*<Route path="/auth" component={Auth} />*!/*/}
+    //         {/*/!* add routes without layouts *!/*/}
+    {/*//         /!*<Route path="/landing" exact component={Landing} />*!/*/}
+    {/*//         /!*<Route path="/profile" exact component={Profile} />*!/*/}
+    {/*        <Route path="${process.env.PUBLIC_URL}/" exact component={Hazard} />*/}
+    {/*//         <Route path="/Tools"  component={Hazard} />*/}
+    {/*//*/}
+    {/*//         /!*<Route path="/Data" exact component={Data} />*!/*/}
+    {/*//         /!*<Route path="/Current" exact component={Current} />*!/*/}
+    {/*//         /!* add redirect for first page *!/*/}
+
+    {/*    </Switch>*/}
+    // </Router>,
+    //
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ texas-damage-plain/precache-manifest.56d7f1ee2971f64350db498d58924614.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fbc27f5bb2038c72c773",
+    "url": "/static/css/main.8dd04487.chunk.css"
+  },
+  {
+    "revision": "fbc27f5bb2038c72c773",
+    "url": "/static/js/main.fbc27f5b.chunk.js"
+  },
+  {
+    "revision": "e2c57f012446fdfe9880",
+    "url": "/static/js/1.e2c57f01.chunk.js"
+  },
+  {
+    "revision": "b6c6a3257cc05b6192a1",
+    "url": "/static/css/2.0adb2710.chunk.css"
+  },
+  {
+    "revision": "b6c6a3257cc05b6192a1",
+    "url": "/static/js/2.b6c6a325.chunk.js"
+  },
+  {
+    "revision": "29c5ccc8813fc127f902",
+    "url": "/static/js/runtime~main.29c5ccc8.js"
+  },
+  {
+    "revision": "0f9ceeceebd06b79d0f44ad1dea09cfd",
+    "url": "/index.html"
+  }
+];
